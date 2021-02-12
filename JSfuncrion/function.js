43c00950c810860f13ea23864a1c9dc11265b12e
MusaPolka <75001112+MusaPolka@users.noreply.github.com>
2021-02-12 09:48:34 +0600

    Add files via upload

---
+++ JSfuncrion/function.js
@@ -0,0 +1,20 @@
+function max(arg1, arg2){
+    var max = Math.max(arg1, arg2)
+    return max;
+}
+
+function min(arg1, arg2){
+    var min = Math.min(arg1, arg2)
+    return min;
+}
+
+function qwad(a, n){
+    var result = Math.pow(a, n)
+    return result;
+}
+
+console.log(max(5, 10));
+
+console.log(min(5, 30));
+
+console.log(qwad(5, 3))
